9cec37e2bfa4038eddcd3a53ae0f32f4d43a99a6
Hrishikesh Kokate <hrishi5200@gmail.com>
2024-12-15 01:11:49 +0800

    switch to json

--- netlify/edge-functions/turnstile.ts
+++ netlify/edge-functions/turnstile.ts
@@ -1,26 +1,24 @@
-import { randomUUID } from 'node:crypto'
 import type { Config, Context } from '@netlify/edge-functions'
 
 export default async function (request: Request, context: Context) {
   const body = await request.clone().formData()
-  const formData = new FormData()
-
-  formData.append('idempotency_key', randomUUID())
-  formData.append('remoteip', context.ip)
-  formData.append('response', body.get('cf-turnstile-response') as string)
-  formData.append('secret', Netlify.env.get('SECRET_KEY') as string)
-  console.log(formData)
 
   const result = await fetch(
     'https://challenges.cloudflare.com/turnstile/v0/siteverify',
     {
-      body: formData,
+      body: JSON.stringify({
+        remoteip: context.ip,
+        response: body.get('cf-turnstile-response'),
+        secret: Netlify.env.get('SECRET_KEY')
+      }),
+      headers: {
+        'content-Type': 'application/json'
+      },
       method: 'POST'
     }
   )
 
   const outcome = await result.json()
-  console.log(outcome)
 
   if (!outcome.success) {
     return Response.json(
